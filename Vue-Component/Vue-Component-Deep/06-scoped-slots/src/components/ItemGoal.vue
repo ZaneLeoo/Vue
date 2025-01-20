a396bac45875998a5737eca9a534d26d7d31e4bb
zaneleoo <zaneleoooooo@gmail.com>
2025-01-20 10:09:28 +0800

    more learning

---
+++ Vue-Component/Vue-Component-Deep/06-scoped-slots/src/components/ItemGoal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <course-goals v-slot="props">
+      <h2>The gola is{{ props.item }}</h2>
+      <h2>Someting -> {{ props.another - prop }}</h2>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import CourseGoals from "./CourseGoals.vue";
+export default {
+  components: {
+    CourseGoals,
+  },
+};
+</script>
+
+<style></style>
